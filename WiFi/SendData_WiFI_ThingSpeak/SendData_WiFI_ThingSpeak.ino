c07fa18da9e0506bfd8e9baae57f38e835088b80
electrorex <151402332+electrorex@users.noreply.github.com>
2024-09-25 23:15:53 -0500

    Update SendData_WiFI_ThingSpeak.ino

--- WiFi/SendData_WiFI_ThingSpeak/SendData_WiFI_ThingSpeak.ino
+++ WiFi/SendData_WiFI_ThingSpeak/SendData_WiFI_ThingSpeak.ino
@@ -44,7 +44,7 @@ void readBat() {
 
 WiFiClient client;
 char ssid[] = "Sawadii5";        //network SSID (name)
-char pass[] = "SongkhlaSidRex";    // network password 
+char pass[] = "xxxxlassss";    // network password 
 
 int status = WL_IDLE_STATUS; 
 
