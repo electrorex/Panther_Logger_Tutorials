348464d3802cb5b42c46d36fc793129c12eb2d77
electrorex <151402332+electrorex@users.noreply.github.com>
2025-03-06 18:05:53 -0600

    Update _03_Post_Functions.ino

--- Cellular/SendData_Cellular_POST/_03_Post_Functions.ino
+++ Cellular/SendData_Cellular_POST/_03_Post_Functions.ino
@@ -4,6 +4,7 @@ void MakeDataString(){
           AirTemp,
           Batv                                                                                      
           );
+  Serial.print("Data String = "); Serial.println(DataString);
 }
 
 void MakePayload(){
@@ -21,13 +22,13 @@ void MakePayload(){
   strcat(Payload, "Connection: keep-alive"); strcat(Payload, "\r\n"); //keep alive
   strcat(Payload, Token); strcat(Payload, "\r\n"); //token
   strcat(Payload, "\r\n"); //required new line
-  strcat(Payload, DataString); strcat(Payload, "\r\n");
+  strcat(Payload, DataString); strcat(Payload, "\r\n\r\n");
 }
 
 void getPayloadCommand(){
   String PayloadS = Payload; 
   int PayloadLength = PayloadS.length();
-  sprintf(PostCommand, "AT+QHTTPPOST=%d,80,80",PayloadLength);
+  sprintf(PostCommand, "AT+QHTTPPOST=%d,120,120",PayloadLength);
 }
 
 void geturlCommand(){
@@ -44,21 +45,51 @@ void setupPOST(){
 }
   
 void PostData(){
+  sendAT("AT+CFUN=1,1", "\r\nAPP RDY", "\r\nERROR", 15000);
+  delay(5000);
+  GetQCSQ();
+  delay(100);
+  GetQCSQ();
+  delay(100);
+  sendAT("ATE1", "\r\nOK", "\r\nERROR", 1000); 
+  delay(100);
+  sendAT("AT+CMEE=2", "\r\nOK", "\r\nERROR", 1000);  //Set error reporting
+  delay(100);
+  sendAT("AT+IFC=2,2", "\r\nOK", "\r\nERROR", 1000);   //set flow control
+  
+ //Wait for registration
+  WaitReg();
+  delay(100);
+  sendAT("AT+CREG=2;+CGREG=2;+CEREG=2", "\r\nOK", "\r\nERROR", 1000); //registration status
+  delay(100);
+  sendAT("AT+CCLK?", "\r\nOK", "\r\nERROR", 2000);  //get the time
+  delay(100);
+  GetQCSQ();                                          //get cell signal
+  sendAT("AT+QHTTPCFG=\"contextid\",1", "\r\nOK", "\r\nERROR", 5000); 
+  delay(100);
+  sendAT("AT+QHTTPCFG=\"responseheader\",1", "\r\nOK", "\r\nERROR", 5000);
+  delay(100);
+  sendAT("AT+QHTTPCFG=\"requestheader\",1", "\r\nOK", "\r\nERROR", 5000);
+  delay(100);
+  sendAT("AT+QIACT?", "\r\nOK", "\r\nERROR", 6000); //Setup GPRS communication
+  delay(100);
+  sendAT("AT+QICSGP=1,1,\"hologram\",\"\",\"\",1", "\r\nOK", "\r\nERROR", 5000); //Set the APN
+  delay(100);
+  sendAT("AT+QIACT=1", "\r\nOK", "\r\nERROR", 10000);
+  delay(100);
+  sendAT("AT+QIACT?", "\r\nOK", "\r\nERROR", 30000); //Setup GPRS communication
+  delay(100);
   sendAT(urlCommand, "\r\nCONNECT", "\r\nERROR", 8000);
   SerialAT.print(url);
   Serial.println(url);
-  delay(1000);
   sendAT("","\r\nOK","\r\nERROR", 30000);
-  delay(1000);
-  sendAT(PostCommand, "\r\nCONNECT", "\r\nERROR", 8000);
+  delay(100);
+  sendAT(PostCommand, "\r\nCONNECT", "\r\nERROR", 60000);
   SerialAT.print(Payload);
   Serial.println(Payload);
-  sendAT("", "\r\nOK", "\r\nERROR", 5000);
-  delay(6000);
-  sendAT("AT", "\r\nOK", "\r\nERROR", 1000);
-  delay(1000);
-  sendAT("AT+QHTTPREAD=60", "+QHTTPREAD: 0", "\r\nERROR", 10000);
-  delay(1000);
-  //sendAT("AT+QIDEACT=1", "\r\nOK", "\r\nERROR", 2000);
-  delay(1000);
+  sendAT("","+QHTTPPOST","\r\nERROR", 30000);
+  sendAT("AT+QHTTPREAD=60", "\r\n+QHTTPREAD: 0", "\r\nERROR", 10000);
+  delay(100);
+  sendAT("AT+QIDEACT=1", "\r\nOK", "\r\nERROR", 2000);
+  delay(100);
 }
