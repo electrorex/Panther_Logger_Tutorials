1a48f7eca67d30bfc537e6ce7f04815219ef51c0
electrorex <151402332+electrorex@users.noreply.github.com>
2025-03-06 22:56:26 -0600

    Update RDO_SDI12.ino

--- RDO_SDI12/RDO_SDI12.ino
+++ RDO_SDI12/RDO_SDI12.ino
@@ -1,19 +1,22 @@
 /*
 This sketch was written for the Electrorex Panther Logger Tutorial 8.
-The sketch reads the In-Situ RDO Pro sensor using SDI12 and parses
-the data into float variables.
+The sketch reads the In-Situ RDO Pro dissolved oxygen sensor using SDI12 
+and parses the data into float variables. Digital pin 11 on the
+Panther Logger is used as the SDI12 port.
 
 Wiring (RDO Pro wires to Panther Logger screw terminal ports):
 White ---> D11
 Red ---> 12VS
 Black ---> GND
+
+-Todd R. Miller, March 6, 2025
 */
 
 //Libraries needed
 #include <SDI12.h>
-#include "Adafruit_MCP23X17.h"
+#include <Panther.h>
 
-Adafruit_MCP23X17 mcp;
+Panther ptr;
 
 #define sdi12pin 11        
 SDI12 sdi12(sdi12pin);
@@ -23,9 +26,7 @@ float DOS;
 float RDOTemp;
  
 void readRDO() {
-    mcp.pinMode(7, OUTPUT);
-    mcp.digitalWrite(7, HIGH); //Turn on 12VS rail
-  
+    ptr.set12v(HIGH); //Power sensor with 12V rail. It needs at least 9V.
     DO = -9999;
     DOS = -9999;
     RDOTemp = -9999;
@@ -37,7 +38,9 @@ void readRDO() {
         sdi12.clearBuffer();
         float RDOData[4] = {0};
         sdi12.clearBuffer();
-        String command1 = "4M!"; //Tell sensor to make a measurement
+       
+        //Tell sensor to make a measurement. Assumes sensor address is 4
+        String command1 = "4M!"; 
         sdi12.sendCommand(command1);
         delay(100);
         String sdiResponse = sdi12.readStringUntil('\n');
@@ -60,45 +63,25 @@ void readRDO() {
         sdi12.end();
     }
   }
-   mcp.pinMode(7, OUTPUT);
-   mcp.digitalWrite(7, LOW); //Turn off 12VS rail
+   ptr.set12v(LOW);
 }
 
 void setup() {
   delay(5000); //wait a bit to get serial monitor up
-  
-  //Start all of the serial ports
   Serial.begin(115200);
+  ptr.begin();
 
   //Turn on red LED1 to indicate the program has started
-  pinMode(13,OUTPUT);
-  digitalWrite(13,HIGH);
+  ptr.LED(1,HIGH);
   
   Serial.println("Setting things up. Please wait");
 
-  Wire.begin();
-  mcp.begin_I2C(0x20);
-
-  mcp.pinMode(4, OUTPUT);
-  mcp.digitalWrite(4, HIGH); //Turn on 3VS rail
+  ptr.set3v3(HIGH);
+  ptr.set12v(HIGH);
   delay(100);
-  
-  mcp.pinMode(7, OUTPUT);
-  mcp.digitalWrite(7, HIGH); //Turn off 12VS rail
-  delay(100);
-  
-  mcp.pinMode(8, OUTPUT);
-  mcp.digitalWrite(8, LOW); //Turn off indicator LED2
-  delay(100);
-  
-  mcp.pinMode(9, OUTPUT);
-  mcp.digitalWrite(9, LOW); //Turn off indicator LED3
-  delay(100);
-  
-  mcp.pinMode(10, OUTPUT);
-  mcp.digitalWrite(10, LOW); //Turn off indicator LED4
-  delay(100);
-  
+  ptr.LED(2,LOW);
+  ptr.LED(3,LOW);
+  ptr.LED(4,LOW);
   Serial.println("Setup finished");
 }
 
